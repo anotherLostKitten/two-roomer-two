0a7bf16a387b2a3556d31f4d2e68000ff7394c5e
Theodore Peters <tpeters@stuy.edu>
2018-10-29 15:21:37 -0400

    comprability

--- enemy.py
+++ enemy.py
@@ -1,1 +1,27 @@
 # enemy ai and stats
+class Enemy():
+    def __init__(r, c):
+        self.r = r
+        self.c = c
+        pass
+    def path(pancake, level, cur, end):
+        if cur == end:
+            return True
+class P():
+    def __init__(c,l):
+        self.r = c[0]
+        self.c = c[1]
+        self.l = l
+    def __eq__(self, other):
+        return self.r + self.c + self.l == other.r + other.c + other.l
+    def __ne__(self, other):
+        return self.r + self.c + self.l != other.r + other.c + other.l
+    def __lt__(self, other):
+        return self.r + self.c + self.l < other.r + other.c + other.l
+    def __gt__(self, other):
+        return self.r + self.c + self.l > other.r + other.c + other.l
+    def __le__(self, other):
+        return self.r + self.c + self.l <= other.r + other.c + other.l
+    def __ge__(self, other):
+        return self.r + self.c + self.l >= other.r + other.c + other.l
+    
