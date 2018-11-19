5179709bb43bd81560485993a789adf38f630201
Theodore Peters <tpeters@stuy.edu>
2018-11-19 15:09:17 -0500

    tweaks...

--- enemy.py
+++ enemy.py
@@ -1,8 +1,18 @@
+from random import randrange
+from heapq import heappush, heappop
+
 from helpers import *
 from player import *
-from heapq import heappush, heappop
-from random import randrange
+import skills
+
 # enemy ai and stats
+
+#    1: +
+#       ^ 
+# 0: +  e  -
+#
+#       -
+
 nogoe = { 0, 7 }
 class Enemy():
     def __init__(self,fileName):
@@ -15,6 +25,7 @@ class Enemy():
         self.sc = int(l[2][l[2].index('x') + 1:])
         self.hp = randrange(*(int(i) for i in l[3].split(' ') if i!=""))
         self.attack = randrange(*(int(i) for i in l[4].split(' ') if i!=""))
+        self.a = skills.q["basic"]
     def fits(self,r,c,p):
         for i in range(self.sr):
             for j in range(self.sc):
@@ -29,7 +40,7 @@ class Enemy():
         while pancake:
             cur = heappop(pancake)
             for i in range(4):
-                tpl = (cur.r+rr(i,0),cur.c+rr(i,1))
+                tpl = (cur.r+rr(i,0),cur.c+rr(i,1)) #  TODO change for different enemy movve types.
                 if tpl not in bancake:
                     bancake[tpl]=(cur.r,cur.c)
                     if p.level.dungeon[tpl[0]+p.r][tpl[1]+p.c] not in nogoe:
@@ -41,25 +52,10 @@ class Enemy():
         if t!=(self.r,self.c):
             return self.bpath(bancake[tpl], bancake, pc) + [t] 
         return []
-        
-class P():
-    def __init__(self,r,c,l):
-        self.r = r
-        self.c = c
-        self.l = l
-    def __eq__(self, other):
-        return abs(self.r) + abs(self.c) + self.l == abs(other.r) + abs(other.c) + other.l
-    def __ne__(self, other):
-        return abs(self.r) + abs(self.c) + self.l != abs(other.r) + abs(other.c) + other.l
-    def __lt__(self, other):
-        return abs(self.r) + abs(self.c) + self.l < abs(other.r) + abs(other.c) + other.l
-    def __gt__(self, other):
-        return abs(self.r) + abs(self.c) + self.l > abs(other.r) + abs(other.c) + other.l
-    def __le__(self, other):
-        return abs(self.r) + abs(self.c) + self.l <= abs(other.r) + abs(other.c) + other.l
-    def __ge__(self, other):
-        return abs(self.r) + abs(self.c) + self.l >= abs(other.r) + abs(other.c) + other.l
-    
+    def cattack(self,p):
+        for i in range(4):
+            for j in self.a()['a']:
+
 if __name__=="__main__":
     a = Enemy("test")
     p = Player()
--- helpers.py
+++ helpers.py
@@ -7,6 +7,24 @@ def rb(i, rc):
 def rc(i, dp):
     return (dp[0] * rr(i, 0) + dp[1] * rr(i, 1), dp[0] * rr(i-1, 0) + dp[1] * rr(i-1, 1))
 
+class P(): # path length comparison class for pathfinding.
+    def __init__(self,r,c,l):
+        self.r = r
+        self.c = c
+        self.l = l
+    def __eq__(self, other):
+        return abs(self.r) + abs(self.c) + self.l == abs(other.r) + abs(other.c) + other.l
+    def __ne__(self, other):
+        return abs(self.r) + abs(self.c) + self.l != abs(other.r) + abs(other.c) + other.l
+    def __lt__(self, other):
+        return abs(self.r) + abs(self.c) + self.l < abs(other.r) + abs(other.c) + other.l
+    def __gt__(self, other):
+        return abs(self.r) + abs(self.c) + self.l > abs(other.r) + abs(other.c) + other.l
+    def __le__(self, other):
+        return abs(self.r) + abs(self.c) + self.l <= abs(other.r) + abs(other.c) + other.l
+    def __ge__(self, other):
+        return abs(self.r) + abs(self.c) + self.l >= abs(other.r) + abs(other.c) + other.l
+
 if __name__ == "__main__":
     print((rr(0, 0), rr(0, 1)))
     print((rr(1, 0), rr(1, 1)))
